04f88e23a685460469c64f5137a038be3ad81a4c
turtlewit@live.com <turtlewit@live.com>
2016-10-26 22:15:49 -0700

    Added engine audio class to handle game audio. Added a script to play music related to the player's current world. Added the ability to add music files to worlds in the world file.

---
+++ AdventureEngine/CoreEngine/audio.py
@@ -0,0 +1,68 @@
+import pygame.mixer
+
+class Audio:
+
+	def __init__(self):
+		try:
+			pygame.mixer.init()
+			self.mixerInit = True
+		except:
+			self.mixerInit = False
+
+		self.m_sounds = {}
+		self.m_songs = {}
+
+		self.m_queue = []
+
+		self.m_volume = 1.0
+
+	def LoadSound(self, name, file):
+		if self.mixerInit:
+			self.m_sounds[name] = pygame.mixer.Sound(file)
+
+	def LoadSong(self, name, file):
+		if self.mixerInit:
+			self.m_songs[name] = file
+
+	def PlaySound(self, name, loops=0):
+		if self.mixerInit:
+			try:
+				self.m_sounds[name].set_volume(self.m_volume)
+				self.m_sounds[name].play()
+			except:
+				pass
+
+	def StopPlayingSound(self, name, fadeout=False, fadeoutTime=1000):
+		if self.mixerInit:
+			try:
+				self.m_sounds[name].stop()
+			except:
+				pass
+
+	def Panic(self):
+		if self.mixerInit:
+			try:
+				pygame.mixer.music.stop()
+			except:
+				pass
+
+			for sound in self.m_sounds:
+				try:
+					sound.stop()
+				except:
+					pass
+
+	def PlaySong(self, name, loops=-1, startTime=0.0):
+		if self.mixerInit:
+			try:
+				pygame.mixer.music.stop()
+			except:
+				pass
+
+			pygame.mixer.music.load(self.m_songs[name])
+			pygame.mixer.music.play(loops, startTime)
+	def StopMusic(self):
+		if self.mixerInit:
+			pygame.mixer.music.stop()
+
+
