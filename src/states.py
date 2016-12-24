87db15b3eb1427adc453c2136ab0d1a276490b15
turtlewit@live.com <turtlewit@live.com>
2016-12-23 17:36:47 -0800

    Update and Fix Licensing

--- src/states.py
+++ src/states.py
@@ -1,3 +1,21 @@
+#------------------------------------------------------------------------------#
+# Copyright 2016-2017 Golden Sierra Game Development Class                     #
+# This file is part of Verloren (GSHS_RPG).                                    #
+#                                                                              #
+# Verloren (GSHS_RPG) is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# Verloren (GSHS_RPG) is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with Verloren (GSHS_RPG).  If not, see <http://www.gnu.org/licenses/>. #
+#------------------------------------------------------------------------------#
+
 from AdventureEngine.components.gamecomponent import GameComponent 
 from AdventureEngine.components.state import State
 from AdventureEngine.CoreEngine.input import Input
