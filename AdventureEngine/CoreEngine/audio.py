653088611153314ca1116f963117df81182f2079
turtlewit@live.com <turtlewit@live.com>
2017-01-29 20:18:24 -0800

    Add Tolerence for SimpleAudio

--- AdventureEngine/CoreEngine/audio.py
+++ AdventureEngine/CoreEngine/audio.py
@@ -15,75 +15,108 @@
 # You should have received a copy of the GNU General Public License            #
 # along with Verloren (GSHS_RPG).  If not, see <http://www.gnu.org/licenses/>. #
 #------------------------------------------------------------------------------#
+playAudio = True
+try:
+	import simpleaudio as sa
+except:
+	playAudio = False
 
-import simpleaudio as sa
+if playAudio:
 
+	class Audio:
 
-class Audio:
-
-	def __init__(self):
-
-
-		self.m_sounds = {}
-		self.m_songs = {}
-		self.m_currentSong = None
-		self.m_currentSongPlayObject = None
-		self.m_currentSoundPlayObjects = {}
-
-		self.m_queue = []
-
-		self.m_volume = 1.0
-
-	def LoadSound(self, name, file):
+		def __init__(self):
 
 
-		self.m_sounds[name] = sa.WaveObject.from_wave_file(file)
-
-	def LoadSong(self, name, file):
-
-
-		self.m_songs[name] = sa.WaveObject.from_wave_file(file)
-
-	def PlaySound(self, name, loops=0):
-
-
-		# Cleanup before playing sound
-		for obj in self.m_currentSoundPlayObjects.copy():
-			if not obj.is_playing():
-				self.m_currentSoundPlayObjects.pop(obj)
-
-
-		self.m_currentSoundPlayObjects[name] = (self.m_sounds[name].play())
-
-	def StopPlayingSound(self, name, fadeout=False, fadeoutTime=1000):
-
-
-		self.m_currentSoundPlayObjects[name].stop()
-		self.m_currentSoundPlayObjects.pop(name)
-
-	def Panic(self):
-
-		for obj in self.m_currentSoundPlayObjects:
-			obj.stop()
-
-		self.m_currentSoundPlayObjects = {}
-
-	def PlaySong(self, name, loops=-1, startTime=0.0):
-
-		if not self.m_currentSongPlayObject:
-			self.m_currentSongPlayObject = self.m_songs[name].play()
-		else:
-			self.m_currentSongPlayObject.stop()
-			self.m_currentSongPlayObject = self.m_songs[name].play()
-
-		self.m_currentSong = name
-
-	def StopMusic(self):
-
-		if self.m_currentSongPlayObject:
-			self.m_currentSongPlayObject.stop()
+			self.m_sounds = {}
+			self.m_songs = {}
+			self.m_currentSong = None
 			self.m_currentSongPlayObject = None
+			self.m_currentSoundPlayObjects = {}
 
-		self.m_currentSong = None
+			self.m_queue = []
+
+			self.m_volume = 1.0
+
+		def LoadSound(self, name, file):
 
 
+			self.m_sounds[name] = sa.WaveObject.from_wave_file(file)
+
+		def LoadSong(self, name, file):
+
+
+			self.m_songs[name] = sa.WaveObject.from_wave_file(file)
+
+		def PlaySound(self, name, loops=0):
+
+
+			# Cleanup before playing sound
+			for obj in self.m_currentSoundPlayObjects.copy():
+				if not obj.is_playing():
+					self.m_currentSoundPlayObjects.pop(obj)
+
+
+			self.m_currentSoundPlayObjects[name] = (self.m_sounds[name].play())
+
+		def StopPlayingSound(self, name, fadeout=False, fadeoutTime=1000):
+
+
+			self.m_currentSoundPlayObjects[name].stop()
+			self.m_currentSoundPlayObjects.pop(name)
+
+		def Panic(self):
+
+			for obj in self.m_currentSoundPlayObjects:
+				obj.stop()
+
+			self.m_currentSoundPlayObjects = {}
+
+		def PlaySong(self, name, loops=-1, startTime=0.0):
+
+			if not self.m_currentSongPlayObject:
+				self.m_currentSongPlayObject = self.m_songs[name].play()
+			else:
+				self.m_currentSongPlayObject.stop()
+				self.m_currentSongPlayObject = self.m_songs[name].play()
+
+			self.m_currentSong = name
+
+		def StopMusic(self):
+
+			if self.m_currentSongPlayObject:
+				self.m_currentSongPlayObject.stop()
+				self.m_currentSongPlayObject = None
+
+			self.m_currentSong = None
+
+else:
+	class Audio:
+		def __init__(self):
+			self.m_sounds = {}
+			self.m_songs = {}
+			self.m_currentSong = None
+			self.m_currentSongPlayObject = None
+			self.m_currentSoundPlayObjects = {}
+
+			self.m_queue = []
+
+			self.m_volume = 1.0
+
+		def LoadSound(self, name, file):
+			pass
+
+		def LoadSong(self, name, file):
+			pass
+
+		def StopPlayingSound(self, name, fadeout=False, fadeoutTime=1000):
+			pass
+
+		def Panic(self):
+			pass
+
+		def PlaySong(self, name, loops=-1, startTime = 0.0):
+			pass
+
+		def StopMusic(self):
+			pass
